da7f1eff90f0582aea5cac4eb992d20cb6a91537
Soudea Forbes <soudeaforbes@gmail.com>
2014-12-05 01:22:18 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//PIE CHART OF CRIMES BY TYPE
+//sort crimes by primary type
+
+if (feature.properties.PrimaryType === ASSAULT | BATTERY |HOMICIDE |INTIMIDATION
+| KIDNAPPING |STALKING)
+then CrimeGroup = "Crimes Against Persons (Not Including Sex Crimes)"
+
+
+if (feature.properties.PrimaryType === SEX OFFENSE) 
