6bd03c15d17638c0484d69142fc7d0e92fb17620
Soudea Forbes <soudeaforbes@gmail.com>
2014-12-16 19:16:10 -0500

    five
    omg i fixed it

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ var Esri_OceanBasemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/
 	attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
 	maxZoom: 13}).addTo(map);
 
-var map = d3.select(map.getPanes().overlayPane).append("svg"),
-    g = map.append("g").attr("class", "leaflet-zoom-hide");
+var mapp = d3.select(map.getPanes().overlayPane).append("svg"),
+    g = mapp.append("g").attr("class", "leaflet-zoom-hide");
 
 d3.json("chicagohomicides2013.geojson", function(collection) {
   var transform = d3.geo.transform({point: projectPoint}),
       path = d3.geo.path().projection(transform);
   var feature = g.selectAll("path")
       .data(collection.features)
-    .enter().append("path").transition().delay(function(d, i) { return 100 * i; } );
-  map.on("viewreset", reset);
+    .enter().append("path").transition().delay(function(d, i) { return 100 * i } );
+  mapp.on("viewreset", reset);
   reset();
   
   // Reposition the SVG to cover the features.
@@ -21,7 +21,7 @@ d3.json("chicagohomicides2013.geojson", function(collection) {
     var bounds = path.bounds(collection),
         topLeft = bounds[0],
         bottomRight = bounds[1];
-    map .attr("width", bottomRight[0] - topLeft[0])
+    mapp.attr("width", bottomRight[0] - topLeft[0])
         .attr("height", bottomRight[1] - topLeft[1])
         .style("left", topLeft[0] + "px")
         .style("top", topLeft[1] + "px");
@@ -29,7 +29,7 @@ d3.json("chicagohomicides2013.geojson", function(collection) {
     feature.attr("d", path);
   }
   
-  // Use Leaflet to implement a D3 geometric transformation.
+ // Use Leaflet to implement a D3 geometric transformation.
   function projectPoint(x, y) {
     var point = map.latLngToLayerPoint(new L.LatLng(y, x));
     this.stream.point(point.x, point.y);
@@ -61,7 +61,7 @@ function makeChart(data){
    .attr("x", 0)
    .attr("height", 53)
    .attr("width", function(d) {
-      return 6*d.Count;
+      return 8*d.Count;
     })
     .attr("fill", function(d) {
     return "rgb(" + (d.Count * 5) + ",0,60)";
@@ -72,7 +72,7 @@ function makeChart(data){
    .enter()
    .append("text")
    .text(function(d) {
-        return d.Month;
+        return d.Month + ": " + d.Count;
    })
    .attr("y", function(d, i) {
         return i * (704/12) +35;
